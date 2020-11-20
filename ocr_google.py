5dabe27e137f3a14eec2205cfc824a00140a5f21
htw-kevkev <67795307+htw-kevkev@users.noreply.github.com>
2020-11-20 18:48:29 +0100

    Completed script for google vision api

--- ocr_google.py
+++ ocr_google.py
@@ -1,42 +1,84 @@
 
+# !!! INTERESSANT: https://www.tarent.de/blog/tesseract-opencvs-east-oder-doch-lieber-google-vision-api
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 20 17:26:00 2020
+
+@author: kevin
+"""
 from google.cloud import vision
 import io
 import os
 import cv2
 import matplotlib.pyplot as plt
+import csv
+import utils
+
+# initialize logger
+log = utils.set_logging()
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="credentials.json"
 
+
+def handle_directory(directory, directory_name):
+    log.info('Start handling directory ' + str(directory_name))
+    
+    try:
+        listOfFiles = os.listdir(directory)
+        total_images = len(listOfFiles)
+        image_counter = 0
+        for file in listOfFiles:
+            image_counter = image_counter + 1
+            path = os.path.join(directory, file).replace('\\','/')
+            ocr_google = detect_text(path, file, total_images, image_counter)
+            row = [directory_name, file, ocr_google]
+            imageRows.append(row)
+    except:
+        log.error('There was a problem handling the directory ' + str(directory_name))
+
+
 # from https://cloud.google.com/vision/docs/ocr#vision_text_detection-python
-def detect_text(path):
-    """Detects text in the file."""
-    from google.cloud import vision
-    import io
-    client = vision.ImageAnnotatorClient()
+def detect_text(path, file, total_images, image_counter):
+    log.info('Start handling image ' + str(image_counter) + ' of ' + str(total_images) + ': ' + str(file))
 
-    with io.open(path, 'rb') as image_file:
-        content = image_file.read()
+    try:
+        """Detects text in the file."""
+        client = vision.ImageAnnotatorClient()
 
-    image = vision.Image(content=content)
+        with io.open(path, 'rb') as image_file:
+            content = image_file.read()
 
-    response = client.text_detection(image=image)
-    texts = response.text_annotations
-    print('Texts:')
+        image = vision.Image(content=content)
 
-    for text in texts:
-        print('\n"{}"'.format(text.description))
+        response = client.text_detection(image=image)
+        texts = response.text_annotations
+        # print('Texts:')
 
-        vertices = (['({},{})'.format(vertex.x, vertex.y)
-                    for vertex in text.bounding_poly.vertices])
+        # first description already contains all text elements
+        ocr_google = texts[0].description
+        ocr_google = ocr_google.replace(',','').replace(';','').replace('|','').replace('\n', '|')
 
-        print('bounds: {}'.format(','.join(vertices)))
+        # for text in texts:
+        #     print(text.description)
 
-    if response.error.message:
-        raise Exception(
-            '{}\nFor more info on error messages, check: '
-            'https://cloud.google.com/apis/design/errors'.format(
-                response.error.message))
+            # print('\n"{}"'.format(text.description))
 
+            # vertices = (['({},{})'.format(vertex.x, vertex.y)
+            #             for vertex in text.bounding_poly.vertices])
+
+            # print('bounds: {}'.format(','.join(vertices)))
+
+        # if response.error.message:
+        #     raise Exception(
+        #         '{}\nFor more info on error messages, check: '
+        #         'https://cloud.google.com/apis/design/errors'.format(
+        #             response.error.message))
+
+        return ocr_google
+    except:
+        log.error('There was a problem handling the image ' + str(file))
+        return 'ERROR'
 
 def show_image(image):
     plt.imshow(image)
@@ -44,15 +86,29 @@ def show_image(image):
     plt.show()
 
 
-dir_test = 'C:/Users/kevin/OneDrive/Studium/4_WiSe20_21/1_W3-WM/app_data/test_images'
+log.info('Inizialize list for image data')
+imageRows = []
 
-listOfFiles = os.listdir(dir_test)
-for file in listOfFiles:
-    print('== Processing file ' + str(file) + ' ==')
-    path = os.path.join(dir_test, file).replace('\\','/')
+# dirtest = 'C:/Users/kevin/OneDrive/Studium/4_WiSe20_21/1_W3-WM/app_data/test_images'
+# dirtest_name = 'test_images'
 
-    detect_text(path)
+dir1 = 'C:/Users/kevin/OneDrive/Studium/4_WiSe20_21/1_W3-WM/app_data/Plakatfotos'
+dir1_name = 'plakatfotos'
 
-    image = cv2.imread(completePath, cv2.IMREAD_COLOR)
-    show_image(image)
+dir2 = 'C:/Users/kevin/OneDrive/Studium/4_WiSe20_21/1_W3-WM/app_data/insta_posters'
+dir2_name = 'insta_posters'
+
+# handle_directory(dirtest, dirtest_name)
+handle_directory(dir1, dir1_name)
+handle_directory(dir2, dir2_name)
+
+csvfilename = 'images_to_text.csv'
+log.info('Write list with image data to file ' + str(csvfilename))
+columns = ['DIRECTORY', 'FILE', 'ocr_google']
+with open(csvfilename, 'w', encoding='utf-8', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(columns)
+    csvwriter.writerows(imageRows)
+
+log.info('All done')
 
